73a0bd729e27982bd065dc149985bc16b5af9f2d
Can Kanbur <cankanbur1@gmail.com>
2022-05-25 12:04:14 +0300

    added sample

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
-
+import text from './data.js';
+import React, { useState } from 'react';
 function App() {
+  const [data, setData] = useState([]);
+  const [count, setCount] = useState(0);
+  const handleSubmit = e => {
+    e.preventDefault();
+    let amount = parseInt(count);
+    if (amount < 0) {
+      amount = 1;
+    }
+    if (amount > text.length) {
+      amount = 8;
+    }
+    setData(text.slice(0, amount));
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="section-center">
+      <h3>Lorem, ipsum dolor.</h3>
+      <form onSubmit={handleSubmit} className="lorem-form">
+        <label htmlFor="amounts">paragraph</label>
+        <input
+          type="number"
+          id="amounts"
+          name="amounts"
+          value={count}
+          onChange={e => setCount(e.target.value)}
+        />
+        <button className="btn">generate</button>
+      </form>
+      <article className="lorem-text">
+        {data.map((item, index) => {
+          return <p key={index}>{item}</p>;
+        })}
+      </article>
+    </section>
   );
 }
 
